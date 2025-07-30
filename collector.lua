8ee8a14d8b97153f81f7506ac741c3a6ca9006c6
slavabeez <slava_berdugin119@mail.ru>
2025-07-31 00:17:35 +0300

    Delete collector.lua

--- collector.lua
+++
@@ -1,8 +0,0 @@
-while(true)do 
-workspace.Pickups:WaitForChild("BeachBall")
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Pickups.BeachBall.CFrame
-wait(3)
-workspace.Pickups.BeachBall:Destroy()
-wait(0.1)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3,3,3)
-end
